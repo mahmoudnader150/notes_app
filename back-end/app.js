13329b037e5adf218c617a7319b6c910e46af227
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2023-12-20 09:15:46 +0200

    install node packages and add mandatory files

---
+++ back-end/app.js
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/newline-after-import */
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable node/no-extraneous-require */
+const express = require('express');
+const morgan = require('morgan');
+const notesRouter = require('./routes/userRoutes');
+const app = express();
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+app.use((req, res, next) => {
+  console.log('Hello from middleware!');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use('/api/notes', notesRouter);
+
+module.exports = app;
---
+++ back-end/server.js
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/newline-after-import */
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+console.log(process.env);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true, 
+   // useUnifiedTopology: true, // Adding the unified topology option
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+  });
+
+
+
+ 
+
+const port = process.env.PORT  ||  8000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+ 
