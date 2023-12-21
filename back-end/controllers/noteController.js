1df18485263970eed01dfd3bff4be4a191861778
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2023-12-21 06:13:33 +0200

    add flutter packages and files

--- back-end/controllers/noteController.js
+++ back-end/controllers/noteController.js
@@ -111,23 +111,9 @@ exports.getNote =  async(req,res)=>{
      } 
  
 
-exports.addNote = (req,res)=>{
-    
-    // const newId = tours[tours.length - 1].id + 1;
-    // const newTour = Object.assign({id:newId},req.body);
  
-    // tours.push(newTour);
-    // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
-    //       res.status(201).json({
-    //            status:'success',
-    //            data:{
-    //              tour:newTour
-    //            }
-    //       });
-    //      });
- }
 
-exports.updateNote =async (req, res)=>{
+exports.updateNote = async (req, res)=>{
      
      try{
         const note = await Note.findByIdAndUpdate(req.params.id, req.body, {
